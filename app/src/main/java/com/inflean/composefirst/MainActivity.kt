4d0d6e4facfbb849da4db7e9e496de5dc237d663
Inwoo <lys91892@gmail.com>
2024-04-06 01:57:57 +0900

    Image(파일, 웹)

--- app/src/main/java/com/inflean/composefirst/MainActivity.kt
+++ app/src/main/java/com/inflean/composefirst/MainActivity.kt
@@ -3,111 +3,50 @@ package com.inflean.composefirst
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.inflean.composefirst.ui.theme.ComposeFirstTheme
 
-// TextField -> EditText 역할
+// Image
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFirstTheme {
-                MyTextField3()
+                MyImageTest2()
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTextField1() {
-
-    var textState by remember { mutableStateOf("Hello") }
-
-    TextField(
-        value = textState,
-        onValueChange = { textState = it }, // it이 아니라 "마마"를 넣는다면 사용자가 입력해도 그 "마마"가 도출됨
-        label = { Text(text = "입력하는 공간") }
+fun MyImageTest1(){
+    Image(
+        painter = painterResource(id = R.drawable.pen),
+        contentDescription = "pen"
     )
-
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTextField2() {
-
-    var textState by remember { mutableStateOf("Hello") }
-
-    OutlinedTextField(
-        value = textState,
-        onValueChange = { textState = it }, // it이 아니라 "마마"를 넣는다면 사용자가 입력해도 그 "마마"가 도출됨
-        label = { Text(text = "입력하는 공간") }
+fun MyImageTest2(){ // 웹에 있는 이미지를 가져올거임
+    AsyncImage(
+        model = "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/612763581/1800",
+        contentDescription = "펭귄사진",
+        modifier = Modifier.fillMaxSize()
     )
-
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MyTextField3() {
-
-    var textState by remember { mutableStateOf("") }
-
-    var enteredText by remember { mutableStateOf("") }
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center, // 세로로 중앙정렬
-        horizontalAlignment = Alignment.CenterHorizontally // 가로로 중앙정렬
-    ) {
-
-        TextField(
-            value = textState,
-            onValueChange = { textState = it },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Button(
-            onClick = {
-                enteredText = textState
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = "입력하기")
-        }
-
-        Text(text = "결과값 텍스트 : ${enteredText}")
-
-    }
-
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeFirstTheme {
-        MyTextField3()
+        MyImageTest2()
     }
 }
