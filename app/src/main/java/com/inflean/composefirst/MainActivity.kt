b7b5f8feefacc33eab75175731b913a3f6e78a03
Inwoo <lys91892@gmail.com>
2024-04-06 00:16:50 +0900

    Text

--- app/src/main/java/com/inflean/composefirst/MainActivity.kt
+++ app/src/main/java/com/inflean/composefirst/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.inflean.composefirst.ui.theme.ComposeFirstTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,11 +29,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyTextEx()
                 }
             }
         }
     }
+}
+
+@Composable()
+fun MyTextEx() {
+    Text(
+        text = "안녕하세요, 텍스트 예제입니다.",
+        fontSize = 30.sp,
+        color = Color.Red,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(30.dp),
+        style = TextStyle(background = Color.Blue)
+    )
 }
 
 @Composable
@@ -44,6 +64,6 @@ fun GreetingPreview() {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Greeting("Android")
+        MyTextEx()
     }
 }
