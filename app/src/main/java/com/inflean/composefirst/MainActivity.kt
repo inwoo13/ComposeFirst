7885ba9c7ef4e68c8c12a98f57b52eea0bdbad8e
Inwoo <lys91892@gmail.com>
2024-04-06 01:11:39 +0900

    Remember

--- app/src/main/java/com/inflean/composefirst/MainActivity.kt
+++ app/src/main/java/com/inflean/composefirst/MainActivity.kt
@@ -1,22 +1,18 @@
 package com.inflean.composefirst
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.inflean.composefirst.ui.theme.ComposeFirstTheme
 
@@ -25,44 +21,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFirstTheme {
-                // A surface container using the 'background' color from the theme
-                MyBtn()
+                SimpleCounterBtn()
             }
         }
     }
 }
 
 @Composable
-fun MyBtn() {
+fun SimpleCounterBtn() {
 
-    val context : Context = LocalContext.current
+    var count by remember {
+        mutableStateOf(0)
+    }
 
-    Button(
-        onClick = {
-            Log.d("Main", "onClick")
-            Toast.makeText(context, "클릭완료", Toast.LENGTH_SHORT).show()
-        },
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Yellow,
-            contentColor = Color.Blue
-        ),
-        modifier = Modifier
-            .width(200.dp)
-            .height(300.dp)
-    ) {
+    Button(onClick = { count++ }, modifier = Modifier.fillMaxSize()) {
         Text(
-            text = "버튼버튼 버튼입니다 버튼버튼버튼버튼",
-            lineHeight = 30.sp, // 줄 띄움 간격
-            fontSize = 30.sp, color = Color.Red
+            text = "Count : $count",
+            fontSize = 50.sp
         )
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeFirstTheme {
-        // A surface container using the 'background' color from the theme
-        MyBtn()
+        SimpleCounterBtn()
     }
 }
