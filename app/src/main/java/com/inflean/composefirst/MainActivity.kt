c097b3ab1db592084b04a7aa4b3f590a190acfab
Inwoo <lys91892@gmail.com>
2024-04-06 01:37:09 +0900

    TextField

--- app/src/main/java/com/inflean/composefirst/MainActivity.kt
+++ app/src/main/java/com/inflean/composefirst/MainActivity.kt
@@ -3,18 +3,24 @@ package com.inflean.composefirst
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,49 +28,86 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.inflean.composefirst.ui.theme.ComposeFirstTheme
 
-// column -> 세로로 줄 세워서 배치
-// spacer / divider
+// TextField -> EditText 역할
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFirstTheme {
-                ColumnTest1()
+                MyTextField3()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ColumnTest1() {
+fun MyTextField1() {
+
+    var textState by remember { mutableStateOf("Hello") }
+
+    TextField(
+        value = textState,
+        onValueChange = { textState = it }, // it이 아니라 "마마"를 넣는다면 사용자가 입력해도 그 "마마"가 도출됨
+        label = { Text(text = "입력하는 공간") }
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextField2() {
+
+    var textState by remember { mutableStateOf("Hello") }
+
+    OutlinedTextField(
+        value = textState,
+        onValueChange = { textState = it }, // it이 아니라 "마마"를 넣는다면 사용자가 입력해도 그 "마마"가 도출됨
+        label = { Text(text = "입력하는 공간") }
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextField3() {
+
+    var textState by remember { mutableStateOf("") }
+
+    var enteredText by remember { mutableStateOf("") }
+
     Column(
-        modifier = Modifier.padding(30.dp)  // 컬럼 자체의 padding
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center, // 세로로 중앙정렬
+        horizontalAlignment = Alignment.CenterHorizontally // 가로로 중앙정렬
     ) {
-        Text(
-            text = "안녕하세요1",
-            fontSize = 30.sp
+
+        TextField(
+            value = textState,
+            onValueChange = { textState = it },
+            modifier = Modifier.fillMaxWidth()
         )
-        Spacer(modifier = Modifier.padding(30.dp))  // 간격 두기
-        Divider(    // 구분선
-            thickness = 4.dp,
-            color = Color.Blue
-        )
-        Text(
-            text = "반갑습니다1",
-            fontSize = 30.sp
-        )
-        Text(
-            text = "반갑습니다2",
-            fontSize = 30.sp
-        )
+
+        Button(
+            onClick = {
+                enteredText = textState
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "입력하기")
+        }
+
+        Text(text = "결과값 텍스트 : ${enteredText}")
+
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeFirstTheme {
-        ColumnTest1()
+        MyTextField3()
     }
 }
