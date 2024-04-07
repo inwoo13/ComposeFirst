2619734a9aa4eeed7179f70a9a6cd6746e3ecdef
Inwoo <lys91892@gmail.com>
2024-04-07 17:20:12 +0900

    Box

--- app/src/main/java/com/inflean/composefirst/MainActivity.kt
+++ app/src/main/java/com/inflean/composefirst/MainActivity.kt
@@ -4,49 +4,79 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.inflean.composefirst.ui.theme.ComposeFirstTheme
 
-// Image
+// Box
+// 적당히 레이아웃 항목을 겹쳐서 내가 원하는 위치에 배치할 수 있음
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFirstTheme {
-                MyImageTest2()
+                BoxExample()
             }
         }
     }
 }
 
 @Composable
-fun MyImageTest1(){
-    Image(
-        painter = painterResource(id = R.drawable.pen),
-        contentDescription = "pen"
-    )
+fun BoxExample(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Red)
+    ){
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(16.dp)
+                .align(Alignment.TopStart)
+        ){
+            Text(text = "왼쪽 위")
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Green)
+                .padding(16.dp)
+                .align(Alignment.TopCenter)
+        ){
+            Text(text = "중앙 위")
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Gray)
+                .padding(16.dp)
+                .align(Alignment.TopEnd)
+        ){
+            Text(text = "오른쪽 위")
+        }
+    }
 }
 
-@Composable
-fun MyImageTest2(){ // 웹에 있는 이미지를 가져올거임
-    AsyncImage(
-        model = "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/612763581/1800",
-        contentDescription = "펭귄사진",
-        modifier = Modifier.fillMaxSize()
-    )
-}
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeFirstTheme {
-        MyImageTest2()
+        BoxExample()
     }
 }
