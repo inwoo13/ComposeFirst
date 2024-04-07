f53f5667e4e77a6448bef69069bdc793a23a02d1
Inwoo <lys91892@gmail.com>
2024-04-07 17:25:30 +0900

    Row

--- app/src/main/java/com/inflean/composefirst/MainActivity.kt
+++ app/src/main/java/com/inflean/composefirst/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,69 +16,49 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.inflean.composefirst.ui.theme.ComposeFirstTheme
 
-// Box
-// 적당히 레이아웃 항목을 겹쳐서 내가 원하는 위치에 배치할 수 있음
+// Row
+// Column과 반대
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFirstTheme {
-                BoxExample()
+                RowTest()
             }
         }
     }
 }
 
 @Composable
-fun BoxExample(){
-    Box(
+fun RowTest() {
+
+    Row(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Red)
-    ){
-        Box(
-            modifier = Modifier
-                .size(100.dp)
-                .background(color = Color.Blue)
-                .padding(16.dp)
-                .align(Alignment.TopStart)
-        ){
-            Text(text = "왼쪽 위")
-        }
-        Box(
-            modifier = Modifier
-                .size(100.dp)
-                .background(color = Color.Green)
-                .padding(16.dp)
-                .align(Alignment.TopCenter)
-        ){
-            Text(text = "중앙 위")
-        }
-        Box(
-            modifier = Modifier
-                .size(100.dp)
-                .background(color = Color.Gray)
-                .padding(16.dp)
-                .align(Alignment.TopEnd)
-        ){
-            Text(text = "오른쪽 위")
-        }
+            .background(color = Color.Gray),
+        horizontalArrangement = Arrangement.SpaceEvenly, // 가로로 동일한 간격
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "item1")
+        Text(text = "item2")
+        Text(text = "item3")
     }
-}
 
+}
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeFirstTheme {
-        BoxExample()
+        RowTest()
     }
 }
