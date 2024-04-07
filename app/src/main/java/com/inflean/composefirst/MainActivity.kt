2944b510c81959efb767ad71c678611bebfa697d
Inwoo <lys91892@gmail.com>
2024-04-07 17:47:15 +0900

    Column+Row

--- app/src/main/java/com/inflean/composefirst/MainActivity.kt
+++ app/src/main/java/com/inflean/composefirst/MainActivity.kt
@@ -3,23 +3,31 @@ package com.inflean.composefirst
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.inflean.composefirst.ui.theme.ComposeFirstTheme
 
@@ -31,25 +39,114 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFirstTheme {
-                RowTest()
+                ColumnRowTest2()
             }
         }
     }
 }
 
 @Composable
-fun RowTest() {
+fun ColumnRowTest1() {
 
-    Row(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Gray),
-        horizontalArrangement = Arrangement.SpaceEvenly, // 가로로 동일한 간격
-        verticalAlignment = Alignment.CenterVertically
+            .padding(20.dp)
+            .background(Color.Gray)
     ) {
-        Text(text = "item1")
-        Text(text = "item2")
-        Text(text = "item3")
+        Text(text = "안녕하세요", color = Color.Blue, fontSize = 20.sp)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            // horizontalArrangement = Arrangement.SpaceBetween    // 완전 벌리는거
+            horizontalArrangement = Arrangement.SpaceAround  // 중앙을 기준으로 간격에 맞게 벌리는거
+            // horizontalArrangement = Arrangement.SpaceEvenly  // 양 끝도 간격 맞추는거
+        ) {
+            Text(text = "왼쪽")
+            Text(text = "중앙")
+            Text(text = "오른쪽")
+        }
+        Text(text = "반갑습니다", color = Color.Blue, fontSize = 20.sp)
+    }
+
+}
+
+@Composable
+fun ColumnRowTest2() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+            .background(Color.Cyan)
+            .border(
+                border = BorderStroke(5.dp, color = Color.Blue)
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Box(
+            modifier = Modifier.padding(top = 20.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.bok),
+                contentDescription = "bok",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(50.dp))
+            )
+        }
+        Text(
+            text = "개복치 개발자",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 50.dp)
+        )
+
+        Text(
+            text = "Developer",
+            fontSize = 15.sp,
+            modifier = Modifier.padding(10.dp)
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+
+            Text(
+                text = "이메일",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+
+            Text(
+                text = "uyalae@naver.com",
+                fontSize = 15.sp,
+                modifier = Modifier.padding(10.dp),
+                color = Color.Blue
+            )
+
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+
+            Text(
+                text = "연락처",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+
+            Text(
+                text = "010-1234-5678",
+                fontSize = 15.sp,
+                modifier = Modifier.padding(10.dp)
+            )
+
+        }
+
     }
 
 }
@@ -59,6 +156,6 @@ fun RowTest() {
 @Composable
 fun GreetingPreview() {
     ComposeFirstTheme {
-        RowTest()
+        ColumnRowTest2()
     }
 }
