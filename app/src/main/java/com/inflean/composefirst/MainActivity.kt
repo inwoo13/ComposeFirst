22df5949cb5b3636fe4511f4a64c34b66c0cbaf8
Inwoo <lys91892@gmail.com>
2024-04-08 01:32:22 +0900

    이력서 앱 - TopBar 생성

--- app/src/main/java/com/inflean/composefirst/MainActivity.kt
+++ app/src/main/java/com/inflean/composefirst/MainActivity.kt
@@ -1,100 +1,16 @@
 package com.inflean.composefirst
 
 import android.os.Bundle
-import android.util.Log
-import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Phone
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DrawerValue
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImage
 import com.inflean.composefirst.ui.theme.ComposeFirstTheme
-import kotlinx.coroutines.launch
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Path
 
 // Canvas
 
@@ -103,48 +19,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFirstTheme {
-                MyCanvas()
+                MyResume()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyCanvas() {
+fun MyResume(){
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .height(500.dp)
-            .background(Color.Red)
-    ) {
-        Canvas(modifier = Modifier
-            .size(200.dp)
-            .align(Alignment.Center)) {
-            drawCircle(Color.Black, radius = size.minDimension / 2)
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Android Dev Resume") })
         }
+    ) {paddingValues ->
+        MyResumeContent(paddingValues)
     }
 
-//    Box(
-//        modifier = Modifier
-//            .width(100.dp)
-//            .height(200.dp)
-//            .background(Color.Green)
-//    ) {
-//        Canvas(modifier = Modifier.fillMaxSize()){
-//            drawCircle(Color.Black, radius = size.minDimension / 2)
-//        }
-//    }
+}
 
-//    Box(
-//        modifier = Modifier
-//            .size(50.dp)
-//            .background(Color.Green)
-//    ) {
-//        Canvas(modifier = Modifier.fillMaxSize()){
-//            drawCircle(Color.Black, radius = size.minDimension / 2)
-//        }
-//    }
+@Composable
+fun MyResumeContent(paddingValues: PaddingValues) {
 
 }
 
@@ -152,6 +48,6 @@ fun MyCanvas() {
 @Composable
 fun GreetingPreview() {
     ComposeFirstTheme {
-        MyCanvas()
+        MyResume()
     }
 }
