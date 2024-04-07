92a4f4b151880e0a07fa44a46c11430814a57b98
Inwoo <lys91892@gmail.com>
2024-04-07 19:46:39 +0900

    ProgressIndicator

--- app/src/main/java/com/inflean/composefirst/MainActivity.kt
+++ app/src/main/java/com/inflean/composefirst/MainActivity.kt
@@ -34,16 +34,22 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -58,63 +64,59 @@ import androidx.compose.ui.viewinterop.AndroidView
 import coil.compose.AsyncImage
 import com.inflean.composefirst.ui.theme.ComposeFirstTheme
 
-// LaxyColumn / LazyRow
-// scrollview 같은 놈임
-// Item Click
+// ProgressIndicator
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeFirstTheme {
-                MyLazyRow()
+                MyProgressIndicator()
             }
         }
     }
 }
 
 @Composable
-fun MyLazyColumnEx() {
+fun MyProgressIndicator() {
 
-    val textList = listOf(
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-    )
+    var progress by remember { mutableStateOf(0.0f) }
 
-    LazyColumn{
-        items(textList){item ->
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Button(onClick = {
+            if (progress <= 1f) {
+                progress += 0.1f
+            } else {
+                progress = 0.0f
+            }
+        }) {
             Text(
-                text = item,
-                fontSize = 60.sp,
-                modifier = Modifier.fillMaxWidth()
+                text = "행복게이지",
+                fontSize = 30.sp
             )
         }
-    }
 
-}
+        Spacer(modifier = Modifier.size(30.dp))
 
-@Composable
-fun MyLazyRow() {
+        LinearProgressIndicator(
+            progress = progress,
+            modifier = Modifier.height(20.dp),
+            color = Color.Red,  // 차는 색
+            trackColor = Color.Cyan // 기본 색
+        )
 
-    val textList = listOf(
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-    )
+        Spacer(modifier = Modifier.size(30.dp))
 
-    LazyRow{
-        items(textList){item ->
-            Text(
-                text = item,
-                fontSize = 100.sp,
-                modifier = Modifier.clickable {
-                    println("Clicked item : $item")
-                }
-            )
-        }
+        CircularProgressIndicator(
+            progress = progress,
+            color = Color.Red
+        )
+
     }
 
 }
@@ -124,6 +126,6 @@ fun MyLazyRow() {
 @Composable
 fun GreetingPreview() {
     ComposeFirstTheme {
-        MyLazyRow()
+        MyProgressIndicator()
     }
 }
